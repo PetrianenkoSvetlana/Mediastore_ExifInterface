c353e002b616267a758c27b44203e4908f182caf
PetrianenkoSvetlana <swetlana.petrianenko@yandex.ru>
2022-12-01 17:04:43 +0300

    Selecting an image, showing it and its Exif tags

--- app/src/main/java/com/example/mediastore_exifinterface/FirstFragment.kt
+++ app/src/main/java/com/example/mediastore_exifinterface/FirstFragment.kt
@@ -1,12 +1,18 @@
 package com.example.mediastore_exifinterface
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.exifinterface.media.ExifInterface
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.mediastore_exifinterface.databinding.FragmentFirstBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -32,13 +38,48 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.imageSelectionBtn.setOnClickListener {
+            pickImage()
         }
+//
+//        binding.editBtn.setOnClickListener {
+//            findNavController().navigate(FirstFragment.action_FirstFragment_to_SecondFragment)
+//        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun pickImage() {
+        // Показываем все программы для запуска
+//        val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*";
+        requestUri.launch(intent)
+    }
+
+    private var requestUri = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val contentResolver = requireContext().contentResolver
+
+        if (result != null && result.resultCode == Activity.RESULT_OK) {
+            result.data?.let { intent ->
+                intent.data?.let { fileUri ->
+                    binding.constraintLayout.visibility = View.VISIBLE
+                    binding.imageView.setImageURI(fileUri)
+                    contentResolver.openInputStream(fileUri)?.use { stream ->
+                        val exif = ExifInterface(stream)
+                        binding.apply {
+                            creationDateTv.text = exif.getAttribute(ExifInterface.TAG_DATETIME)
+                            latitudeTv.text = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE) + ' ' + exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF)
+                            longitudeTv.text = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE) + ' ' + exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF)
+                            creationDeviceTv.text = exif.getAttribute(ExifInterface.TAG_MAKE)
+                            modelCreationDeviceTv.text = exif.getAttribute(ExifInterface.TAG_MODEL)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
